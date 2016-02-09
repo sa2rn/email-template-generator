75e521253ce78214e4c6e8e8af09ab3afe6f64ab
Alexandr Stepanenko <sa2rn.set@gmail.com>
2016-02-09 23:25:12 +0200

    add send email

---
+++ README.md
@@ -0,0 +1,19 @@
+# Generate email template
+
+## Installation
+1. Install [node.js](https://nodejs.org/en/)
+2. [Download archive](https://github.com/sa2rn/email-template-generator/archive/master.zip). And extract it.
+3. Run `npm install`
+
+## Configure
+Copy config.json.dist to config.json and configure it.
+
+## Tasks
+- `gulp` - Run [browsersync](https://www.browsersync.io/) to test your templates in different devices
+- `gulp build` - Build templates
+- `gulp send:index` - Send email. Where "index" is your template name
+
+## Recommendation
+
+You can use [Foundation for Emails](http://foundation.zurb.com/emails/docs.html).
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 var gulp = require('gulp');
 var inlineCss = require('gulp-inline-css');
+var taskListing = require('gulp-task-listing');
 var browserSync = require('browser-sync').create();
 var nodemailer = require('nodemailer');
 var del = require('del');
 var fs = require('fs');
+var glob = require('glob');
+var path = require('path');
+var _ = require('lodash');
 
-var conf = {
-    html: './src/*.html',
-    css: './src/*.css',
-    dist: './dist/'
-};
+var conf = require('./config.json');
+
+var transport = nodemailer.createTransport(conf.smtp);
+
+gulp.task('help', taskListing);
 
 gulp.task('build', function() {
     return gulp.src(conf.html)
@@ -25,6 +29,23 @@ gulp.task('clean', function() {
     del(conf.dist);
 });
 
+// array of template paths
+var templates = glob.sync(conf.html);
+
+// array of task names
+var taskList = templates.map(function(file) {
+    var parsedFile = path.parse(file);
+
+    // create new task 4 template
+    gulp.task('send:' + parsedFile.name, ['build'], function() {
+        sendTemplate(parsedFile.base);
+    });
+
+    return parsedFile.name;
+});
+
+gulp.task('send', taskList);
+
 gulp.task('serve', function() {
     browserSync.init({
         server: conf.dist
@@ -34,3 +55,19 @@ gulp.task('serve', function() {
 });
 
 gulp.task('default', ['build', 'serve']);
+
+// Send email
+function sendTemplate(basename) {
+    fs.readFile(conf.dist + '/' + basename , 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        var options = _.assignIn(conf.email, {'subject': 'Template: ' + basename, 'html': data});
+        transport.sendMail(options, function(error, info){
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: ' + info.response);
+        });
+    });
+}
